991119957240b8d8a36a765fc2909b631d9fe538
shaikhmohseen <shaikhmr7786@gmail.com>
2022-12-22 11:30:12 +0530

    script

--- deploy-ecs-task-defination.sh
+++ deploy-ecs-task-defination.sh
@@ -4,4 +4,7 @@ CLUSTER_NAME=$1
 SERVICE_NAME=$2
 TASK_DEFINATION=$3
 
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $TASK_DEFINATION --desired-count 1
+taskArn=$(grep -oP '(?<="taskDefinitionArn": ")[^"]*' $TASK_DEFINATION | head -1)
+echo $taskArn
+
+aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $taskArn --desired-count 1
